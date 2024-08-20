74b619219d9ddf65272b64eb8288ac401daae5bc
SauloMiller <saulomiller1215@hotmail.com>
2024-08-20 17:19:06 -0300

    lernumero2

---
+++ desafios/lernumero1.py
@@ -0,0 +1,12 @@
+n = int(input(f"Digite um Numero:"))
+
+u = n // 1 % 10
+d = n // 10 % 10
+c = n // 100 % 10
+m = n // 1000 % 10
+
+print(f"Analisando  o numero: {n}")
+print(f"Unidades:{u}")
+print(f"Dezena:{d}")
+print(f"Centena:{c}")
+print(f"Milhar:{m}")
